b897e1018f1c02478ccae3f5e8fd259bbdb2cbb0
grind-t <grindt.gd@gmail.com>
2025-03-29 15:26:44 +0300

    build: init repo

---
+++ lib/index.test.ts
@@ -0,0 +1,6 @@
+import { it, expect } from "vitest";
+import { abc } from ".";
+
+it("should", () => {
+  expect(abc(1, 2)).toBe(3);
+});
---
+++ lib/index.ts
@@ -0,0 +1,3 @@
+export function abc(a: number, b: number): number {
+  return a + b;
+}
